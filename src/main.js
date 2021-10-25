4bf63ec18b10c0fd0e67f0dcfb6e4da1d1579461
ritmillio <f.zoltan.dev@gmail.com>
2021-10-26 00:23:33 +0200

    scrollreveal added to the root

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import '/src/index.css'
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+/* example for usage of scrollreveal*/
+/*
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    // reset: true,
+})
+sr.reveal(`.home__data, .home__social-link, .home__info,
+           .discover__container,
+           .experience__data, .experience__overlay,
+           .place__card,
+           .sponsor__content,
+           .footer__data, .footer__rights`,{
+    origin: 'top',
+    interval: 100,
+})
+*/
